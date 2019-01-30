906b0bf2a32909e6a7e36c83d050ad72832707e8
wujo <wujo635@gmail.com>
2019-01-30 18:19:49 -0500

    Initial commit, project setup with sanity check

---
+++ src/test/java/pillar/com/BabysitterCalculatorTest.java
@@ -0,0 +1,12 @@
+package pillar.com;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BabysitterCalculatorTest {
+
+    @Test
+    public void sanityCheck() {
+        assertEquals(0,-1);
+    }
+}
