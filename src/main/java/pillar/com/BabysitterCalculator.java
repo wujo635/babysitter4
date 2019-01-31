dcaefa4c2b7e4881b4f10e299d1f4acf8c837c0f
wujo <wujo635@gmail.com>
2019-01-31 00:15:52 -0500

    Pass start time as late as 4am

--- src/main/java/pillar/com/BabysitterCalculator.java
+++ src/main/java/pillar/com/BabysitterCalculator.java
@@ -4,7 +4,7 @@ public class BabysitterCalculator {
 
 
     public int calculate(int start, int end) {
-        if (start < 17) {
+        if (start < 17 && start > 4) {
             return -1;
         }
         return 0;
