757d9009714d4f51cad8adcadf8e4dce67fe1bdc
wujo <wujo635@gmail.com>
2019-01-31 00:16:42 -0500

    Refactor method to check if time is within working hours

--- src/main/java/pillar/com/BabysitterCalculator.java
+++ src/main/java/pillar/com/BabysitterCalculator.java
@@ -4,9 +4,13 @@ public class BabysitterCalculator {
 
 
     public int calculate(int start, int end) {
-        if (start < 17 && start > 4) {
+        if (isWithinWorkingHours(start)) {
             return -1;
         }
         return 0;
+    }
+
+    private boolean isWithinWorkingHours(int start) {
+        return start < 17 && start > 4;
     }
 }
