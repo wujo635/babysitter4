8c18eef0baf6ab8dc0bb1e5aeffa83e9283485be
wujo635 <wujo635@gmailc.om>
2019-02-02 20:17:09 -0500

    Pass midnight crossing end before start

--- src/main/java/pillar/com/BabysitterCalculator.java
+++ src/main/java/pillar/com/BabysitterCalculator.java
@@ -11,14 +11,17 @@ public class BabysitterCalculator {
     }
 
     private boolean isValidWorkingHours(int start, int end) {
-        return isWithinWorkingHours(start) || isWithinWorkingHours(end) || isEndBeforeStart(start, end);
+        return isOutsideWorkingHours(start) || isOutsideWorkingHours(end) || isEndBeforeStart(start, end);
     }
 
     private boolean isEndBeforeStart(int start, int end) {
+        if (start <= 4 && end >= 17) {
+            return true;
+        }
         return end < start;
     }
 
-    private boolean isWithinWorkingHours(int time) {
+    private boolean isOutsideWorkingHours(int time) {
         return time < 17 && time > 4;
     }
 }
