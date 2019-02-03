318ccc3e1030b650ea05d2571b6ea238095df392
wujo635 <wujo635@gmailc.om>
2019-02-02 19:57:54 -0500

    Refactor checks for valid working hours to separate method

--- src/main/java/pillar/com/BabysitterCalculator.java
+++ src/main/java/pillar/com/BabysitterCalculator.java
@@ -4,10 +4,14 @@ public class BabysitterCalculator {
 
 
     public int calculate(int start, int end) {
-        if (isWithinWorkingHours(start) || isWithinWorkingHours(end) || isEndBeforeStart(start, end)) {
+        if (isValidWorkingHours(start, end)) {
             return -1;
         }
         return (end - start);
+    }
+
+    private boolean isValidWorkingHours(int start, int end) {
+        return isWithinWorkingHours(start) || isWithinWorkingHours(end) || isEndBeforeStart(start, end);
     }
 
     private boolean isEndBeforeStart(int start, int end) {
